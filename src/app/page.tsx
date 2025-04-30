ddc345867c9012d3a763d36b92d980206980f614
YUMNANASIR01 <yumnanasir719@gmail.com>
2025-04-30 10:41:12 +0500

    First

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import LoginForm from './components/LoginForm'
-import TestComponent from './components/TestComponent'
-import Mycomponent from './components/Mycomponent'
+
 
 function page() {
   return (
